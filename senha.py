6a3d9c29201b3d2da029a400197ef99b74f80401
João Victor <61914542+areamihk@users.noreply.github.com>
2023-11-14 23:30:09 -0300

    senha.py
    Gerador de senhas aleatórias em Python

--- senha.py
+++ senha.py
@@ -3,7 +3,7 @@ import random
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
-symbols = "[]{}()*/;,.-_"
+symbols = "#@%[]{}()*/;,.-_"
 
 all = lower + upper + numbers + symbols
 length = 16
